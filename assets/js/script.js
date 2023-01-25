0696033ed48ea20f50fb1aec059587a8b0681ce8
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-01-25 05:50:01 -0500

    added timer and view high score in html

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var correctAnswer1 = document.querySelector("#correct1");
+var incorrecAnswer1 = document.querySelector("#incorrect1");
+
+function correctAnswer1(){
+    if()
+}
+
+
+
+
+/*var question1ans= 
+
+if (!== "Strings" &&  !== "alerts" &&  !== "Numbers") {
+    ("incorrect");
+    */

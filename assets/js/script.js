a9b38b271a271b3bf84eeb17363acbe82a7dd5f6
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-01-25 06:17:00 -0500

    added functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
 let startButton = document.getElementById("start-button");
+let startDiv = document.getElementById("start-section");
+let questionsDiv = document.getElementById("questions-section");
+let question1 = document.getElementById("question1");
+let question2 = document.getElementById("question2");
+let question3 = document.getElementById("question3");
+let question4 = document.getElementById("question4");
+let question5 = document.getElementById("question5");
+let timeEl = document.getElementById ("timer");
+let timerId; 
+let timeRemaining = 150;
 
 startButton.addEventListener("click", startTheQuiz);
 
 function startTheQuiz (){
-    console.log ("inside start the quiz");
+    startDiv.setAttribute("class", "hide");
+    questionsDiv.removeAttribute("class");
+
+    timerId = setInterval(oneSecondHandler, 1000);
+    timeEl.textContent = timeRemaining;
+    showQuestion ();
 }
+
+function oneSecondHandler (){
+    // to be completed
+}
+
+function showQuestion (){
+    // to be completed
+}
+
